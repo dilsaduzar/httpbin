6ef03cd5f2b681c5224992fd0492767656c85659
dilsad <uzardilsad@gmail.com>
2022-10-21 14:02:21 +0300

    Created userAgent.go and userAgent_test.go files

---
+++ userAgent.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type userResponse struct {
+	UserAgent string `json:"user-agent"`
+}
+
+func userAgentHandler(w http.ResponseWriter, r *http.Request) {
+	uAgent := userResponse{
+		UserAgent: r.UserAgent(),
+	}
+
+	uOut, err := json.MarshalIndent(uAgent, "", "  ")
+	if err != nil {
+		io.WriteString(w, errMsg("Error Code: U"))
+		return
+	}
+	io.WriteString(w, string(uOut))
+}
