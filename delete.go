24e07e2a1ea1f2c3ffaf19eda1ec99ca822b143c
dilsad <uzardilsad@gmail.com>
2022-10-24 11:40:57 +0300

    Added method that delete. Created delete.go and delete_test.go files

---
+++ delete.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type errDelete struct {
+	ErrMsg string
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "DELETE":
+		out, err := io.ReadAll(r.Body)
+		if err != nil {
+			io.WriteString(w, errMsg("Error Code: D01"))
+			return
+		}
+		io.WriteString(w, string(out))
+	default:
+		w.WriteHeader(501)
+		io.WriteString(w, errMsg("Supports only DELETE method. Please use DELETE method."))
+		return
+	}
+}
