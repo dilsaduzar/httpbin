1f9fef3850108c05e37de6bd753ebf562a7a2bc4
dilsad <uzardilsad@gmail.com>
2022-10-31 15:16:27 +0300

    Added files

--- server.go
+++ server.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
 
+	"github.com/dilsaduzar/httpbin/handler"
 	"github.com/gorilla/mux"
 )
-
-type msgErr struct {
-	ErrMsg string
-}
 
 func main() {
 	port := flag.String("port", "", "port number of the running server")
@@ -23,34 +19,26 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/", handler.RootHandler)
 
-	r.HandleFunc("/echo/{name}", echoHandler)
-	r.HandleFunc("/status/{codes}", statusHandler)
+	r.HandleFunc("/echo/{name}", handler.EchoHandler)
+	r.HandleFunc("/status/{codes}", handler.StatusHandler)
 
 	// client requests
-	r.HandleFunc("/ip", ipHandler)
-	r.HandleFunc("/user-agent", userAgentHandler)
-	r.HandleFunc("/headers", headersHandler)
+	r.HandleFunc("/ip", handler.IpHandler)
+	r.HandleFunc("/user-agent", handler.UserAgentHandler)
+	r.HandleFunc("/headers", handler.HeadersHandler)
 
 	// methods
-	r.HandleFunc("/get", getHandler)
-	r.HandleFunc("/post", postHandler)
-	r.HandleFunc("/put", putHandler)
-	r.HandleFunc("/delete", deleteHandler)
-	r.HandleFunc("/patch", patchHandler)
+	r.HandleFunc("/get", handler.GetHandler)
+	r.HandleFunc("/post", handler.PostHandler)
+	r.HandleFunc("/put", handler.PutHandler)
+	r.HandleFunc("/delete", handler.DeleteHandler)
+	r.HandleFunc("/patch", handler.PatchHandler)
 
 	fmt.Printf("Starting server: %s\n", *port)
 	err := http.ListenAndServe(fmt.Sprintf(":%s", *port), r)
 	if err != nil {
 		fmt.Printf("Couldn`t start server. Error %s\n", err)
 	}
-}
-func errMsg(msg string) string {
-	Msg := msgErr{msg}
-	outErr, err := json.Marshal(&Msg)
-	if err != nil {
-		return `{"Error code": "-1"}`
-	}
-	return string(outErr)
 }
